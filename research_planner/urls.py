4dfd5ca4127c06a9ff75dbc418886f40f43af611
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2023-03-17 01:08:27 +0000

    serve static files when Debug=False

--- research_planner/urls.py
+++ research_planner/urls.py
@@ -3,13 +3,14 @@ Definition of urls for iBeatStudyWebApp.
 """
 
 from datetime import datetime
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import views as auth_views
 from jobs import forms, views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve 
 # Use include() to add URLS from the  authentication system
 from django.urls import include
 
@@ -44,11 +45,13 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='jobs/password/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="jobs/password/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='jobs/password/password_reset_complete.html'), name='password_reset_complete'),      
-    path("password_reset", views.password_reset_request, name="password_reset")
+    path("password_reset", views.password_reset_request, name="password_reset"),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
 #Add Django site authentication urls (for login, logout, password management)
 #urlpatterns += [
  #   path('accounts/', include('django.contrib.auth.urls')),]
 
 #if settings.DEBUG:
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
