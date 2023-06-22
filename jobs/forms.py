a7ea1f2659f471386addd62a59a31bfe821eebb1
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2023-06-22 15:53:59 +0100

    added comments

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,5 +1,10 @@
 """
-Definition of forms.
+This module defines structured and validated forms for collecting user input.
+Forms in Django come with built-in validation capabilities. 
+You can specify validation rules for each field, such as required fields, 
+maximum length, numeric constraints, and custom validations. 
+When a user submits a form, Django automatically validates the input based 
+on these rules and provides error messages if the data is invalid.
 """
 
 from django import forms
@@ -10,6 +15,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .modules.SendEmail import SendEmail
 
+#Log in form
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
     username = forms.CharField(max_length=254,
@@ -21,8 +27,11 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
 
+
 #Form for user self-registration
 class NewUserForm(UserCreationForm):
+	#This class inherits from UserCreationForm, adds an email field and 
+	#sends a welcome email to the new user when they are added to the database.
 	email = forms.EmailField(required=True)
 
 	class Meta:
