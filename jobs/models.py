1f7985950770c52fb339ae114783b1e490bb9710
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2023-06-22 13:05:19 +0100

    Improved comments

--- jobs/models.py
+++ jobs/models.py
@@ -1,8 +1,22 @@
 """
-Definition of models.
+Definition of models using Django's ORM (Object-Relational Mapping) to interact with the database.
 Each class represents a database table.  
-Each class is 'migrated' to the database 
-and a table constructed using the class definition.
+Each class is 'migrated' to the database and a table constructed using the class definition.
+
+Whenever one of the model classes is changed, open a command prompt in the root of this project and
+run,
+        python manage.py makemigration
+
+This command analyzes your models and creates migration files in the migrations directory 
+of each app involved in the changes. 
+The migration files contain Python code that represents the changes to be made 
+in the database schema.
+
+If the above command ran without generating errors, then run,
+        python manage.py migrate
+
+It will create new tables, modify existing tables, and apply any necessary data transformations 
+to keep the database schema in sync with your models.
 """
 
 from django.db import models
@@ -10,7 +24,7 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
-    """Model representing a task performed on a dataset."""
+    """Model representing a task performed on a dataset. It defines the Task table in the database"""
     objects = models.Manager() #Not necessary but without it this function fails PyLint
     task_id = models.AutoField(primary_key=True)
     task_name = models.CharField(max_length=50, blank=False, null=False)
@@ -18,11 +32,11 @@ class Task(models.Model):
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.task_name}' # This always returns string even if self.name is None
+        return f'{self.task_name}' # This always returns string even if self.task_name is None
 
 
 class Patient(models.Model):
-    """Model representing a patient."""
+    """Model representing a patient. It defines the Patient table in the database."""
     objects = models.Manager() #Not necessary but without it this function fails PyLint
     patient_id = models.CharField(primary_key=True, max_length=30, default="patient_1")
 
@@ -32,6 +46,10 @@ class Patient(models.Model):
 
 
 class Job(models.Model):
+    """
+    Model representing a job, the action of a task on a patient's data. 
+    It defines the Job table in the database.
+    """
     objects = models.Manager() #Not necessary but without it this function fails PyLint
     TYPE_OF_STATUS = [('Available', 'Available'),
         ('Not Available', 'Not Available'),
@@ -40,20 +58,22 @@ class Job(models.Model):
         ('Approved', 'Approved')]
     YES_NO = [('yes','yes'), ('no','no')]
     id = models.AutoField(primary_key=True)
+    #the link to the user in the User table
     user_id = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True)
+    #the link to the patient in the Patient table 
     patient_id = models.ForeignKey('Patient', on_delete=models.PROTECT)
+    #the link to the task in the Task table
     task_id = models.ForeignKey('Task', on_delete=models.PROTECT, null=True, blank=True)
-    repetition_num = models.IntegerField(default=1)
+    repetition_num = models.IntegerField(default=1) #Number of times a task should be performed
     status = models.CharField(max_length=13, choices=TYPE_OF_STATUS, default='Available')
     report_name = models.CharField(max_length=52, null=True, blank=True)
-    start_date = models.DateField(blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True) 
     deadline_date = models.DateField(blank=True, null=True)
     submission_date = models.DateField(blank=True, null=True)
     reminder_sent = models.CharField(max_length=3, choices=YES_NO, default='no', blank=False, null=False)
 
     def __str__(self):
         """String for representing the Model object."""
-        #task = Task.objects.get(task_id=self.task_id)
         return f'{self.id}, {self.patient_id}, {self.task_id}, {self.repetition_num}'
 
     class Meta:
@@ -61,6 +81,12 @@ class Job(models.Model):
         verbose_name_plural = "Jobs"
 
 class Configuration(models.Model):
+    """
+    Model defining the Configuration table, which allows the application user to configure 
+        text on the home page,
+        number of days to complete a task,
+        maximum number of 'In Progress' task a user may have at one time.
+    """
     objects = models.Manager() #Not necessary but without it this function fails PyLint
     id = models.AutoField(primary_key=True)
     main_title = models.CharField(max_length=50, blank=False, null=False, default="string stored in database", verbose_name="Project title")
